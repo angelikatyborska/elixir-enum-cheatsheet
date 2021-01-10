8c1d2e9fcda4ab1f5c5f88973f9a9e1a4904f46e
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-01-10 13:38:52 +0100

    Prepare deployment

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,5 @@
-const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: ['/'],
-      }),
-    ],
-  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/elixir-enum-cheatsheet/'
+    : '/',
 };
